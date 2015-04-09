b5723ee7ae211ee519cfa022b5a0f611fa3d0c85
Julia Evans <julia@jvns.ca>
2015-04-08 22:43:46 -0400

    french website

---
+++ french_website.py
@@ -0,0 +1,22 @@
+from flask import Flask, g, request, make_response
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    language = request.headers.get('Accept-Language', '')
+    if 'en' in language:
+        body = "Hello, welcome to PyCon! =D =D so fun"
+    else:
+        body= u"Bonjour, bienvenue \xe0 PyCon!"
+    resp = u"""
+    <html>
+    <body>
+    <h1>{body}</h1>
+    </body>
+    </html>
+    """.format(body=body)
+    return make_response(resp)
+
+app.run(debug=True)
